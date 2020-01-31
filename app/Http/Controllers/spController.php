ccbcbd01450a7d864ee00eb0c342d1d1c3e8e1a9
mfgh <mjt.fallah@gmail.com>
2020-01-31 19:48:59 +0330

    spcontroller modified..

--- app/Http/Controllers/spController.php
+++ app/Http/Controllers/spController.php
@@ -8,7 +8,7 @@ class spController
 {
     public function execSP($id)
     {
-        DB::select('EXEC  Usp_CreateQuota(?)' , array($id) );
+        DB::select('EXEC  Usp_CreateQuota ?' , [$id]);
         return redirect()->with('success', 'اطلاعات با موفقیت ویرایش شد'); //->route('period.edit', $id)->with('success', 'اطلاعات با موفقیت ویرایش شد');
 
     }
